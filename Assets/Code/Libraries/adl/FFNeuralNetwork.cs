9d36593821f0623b1bbdbda214863b592340665e
aamithompson <aamithompson@gmail.com>
2021-02-10 10:01:01 -0800

    Feedfoward Neural Network Update 1

--- Assets/Code/Libraries/adl/FFNeuralNetwork.cs
+++ Assets/Code/Libraries/adl/FFNeuralNetwork.cs
@@ -2,7 +2,7 @@
 // Filename: Calculus.cs
 // Author: Aaron Thompson
 // Date Created: 12/9/2020
-// Last Updated: 2/9/2021
+// Last Updated: 2/10/2021
 //
 // Description:
 //==============================================================================
@@ -21,17 +21,80 @@ public class FFNeuralNetwork {
 	
 // CONSTRUCTORS
 //------------------------------------------------------------------------------
+	public FFNeuralNetwork() {
+		network = new List<List<Neuron>>();
+		weights = new List<Matrix<float>>();
+	
+		//Input and output layers default is identity layer with 1 unit each.
+		AddLayer(ActivationFunctions.Identity,-1, 2);
+	}
+
+// DATA MANAGEMENT
+//------------------------------------------------------------------------------
+	public void AddLayer(System.Func<float, float> f, int i=-1, int n=1, int width=1) {
+		if(i == -1){
+			network.Add(new List<Neuron>());
+			i = network.Count - 1;
+		} else {
+			network.Insert(i, new List<Neuron>());
+		}
+
+		network[i].Add(new Neuron(f));
+		AddUnit(i, width-1);
+
+		if(n > 1) {
+			AddLayer(f, i, n-1, width);
+		}
+	}
+	
+	//Does not allow for the removal of the input OR output layer
+	public void RemoveLayer(int i=-1) {
+		
+	}
+
+	public void AddUnit(int i, int n=1) {
+		System.Func<float, float> f = network[i][0].f;
+		for(int j = 0; j < n; j++) {
+			network[i].Add(new Neuron(f));
+		}
+	}
+
+	//Will not remove a unit if layer width is less than or equal to 1
+	public void RemoveUnit(int i) {
+
+	}
+
+	public void SetLayerWidth(int i, int n) {
+
+	}
+
+	public int GetLayerWidth(int i) {
+			return network[i].Count;
+	}
+	
+	//Does not allow for the input layer's function to be changed
+	public void SetLayerFunction(int i, System.Func<float, float> f) {
+
+	}
+
+// ACTIVATION
+//------------------------------------------------------------------------------
+	public Vector<float> Activate(Vector<float> input) {
+		Vector<float> output = Vector<float>.Zeros(GetLayerWidth(0));
+		
+		return output;
+	}
 
 // PERCEPTRON/NEURON/UNIT
 //------------------------------------------------------------------------------
 	public class Neuron {
-			private System.Func<Vector<float>, float> f;
+			public System.Func<float, float> f;
 
-			public Neuron(System.Func<Vector<float>, float> f) {
+			public Neuron(System.Func<float, float> f) {
 				this.f = f;
 			}
 
-			public float Activate(Vector<float> v) {
+			public float Activate(float v) {
 				return f(v);
 			}
 	}
